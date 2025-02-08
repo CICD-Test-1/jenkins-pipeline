a7d2ad52d0a83239deb2b35c9c6b51865a8655ae
pradeeban y <61871682+pradeebancse@users.noreply.github.com>
2025-02-08 16:25:57 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+stage('custom test stage') {
+            steps {
+                echo "------------------ This is a custom script for testing purpose ----------------------"
+            }
+        }
